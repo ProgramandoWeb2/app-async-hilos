0f5f6405fb289ca89f0a0b87969e711fbbdc0266
ProgramandoWeb2 <programacionwebcibertec@gmail.com>
2024-09-18 19:57:20 -0500

    init project

---
+++ src/main/java/pe/edu/cibertect/app_async_hilos/AppAsyncHilosApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertect.app_async_hilos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAsyncHilosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAsyncHilosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertect/app_async_hilos/AppAsyncHilosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertect.app_async_hilos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAsyncHilosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
